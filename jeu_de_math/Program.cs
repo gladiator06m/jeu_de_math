f1c060cbeab070d2a80de4878d92bb64047545bc
gladiator06m <n.khedaiji@gmail.com>
2021-03-07 21:03:51 +0100

    first commit

---
+++ jeu_de_math/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace jeu_de_math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(3+6);
+
+        }
+    }
+}
